ec5b53193638ae8e7505953c97f015277f7ccf18
静文（jingwen） <jingwen@xiaoshijie.com>
2018-03-27 18:03:14 +0800

    feature

---
+++ pages/getCode/index.js
@@ -0,0 +1,86 @@
+// pages/getCode/index.js
+import util from "../../utils/util.js";
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    code: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+    this.getCode();
+  },
+  /*
+   * 获取用户授权
+   */
+  getCode:function(){
+    let self = this;
+
+    util.getCode().then(res => {
+      if (res.hasOwnProperty('code')) {
+        self.setData({
+          "code": res.code
+        })
+      } else {
+        self.setData({
+          "code": '已经授权'
+        })
+      }
+    });
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ pages/userInfo/index.js
@@ -0,0 +1,79 @@
+// pages/userInfo/index.js
+import util from "../../utils/util.js";
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo: {}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getSetting();
+  },
+  /*
+   * 获取用户信息
+   */
+  getSetting: function () {
+    let seft = this;
+    util.getSetting('scope.userInfo').then(res => {
+      console.log(res);
+      this.setData({
+        'userInfo': res
+      })
+    });
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
